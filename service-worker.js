c05f64a8874f806d398adfdfcbddadadcbfc8263
san-woori <kazer35@gmail.com>
2026-07-15 18:10:23 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'workout-journal-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// 서비스 워커 설치 및 파일 캐싱
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// 네트워크 요청 시 캐시 우선 사용
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
